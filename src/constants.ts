43581a0e6fef580e6fc0887e8aead298ee3151d0
Tomer Shoham <tshoham@fireblocks.com>
2026-01-18 14:58:40 +0200

    feat: add minimum recipient and change lovelace constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 export const iagonBaseUrl = "https://api.fireblocks.partners.iagon.com";
 export const tokenTransactionFee = 200000;
+export const MIN_RECIPIENT_LOVELACE = 1_200_000;
+export const MIN_CHANGE_LOVELACE = 1_200_000;
