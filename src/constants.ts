b26cc69ec00833309212ae993b7946de25d0fdd0
Tomer Shoham <tshoham@fireblocks.com>
2026-01-25 11:16:20 +0200

    feat: add staking deposit constants for ADA transactions

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,6 @@ export const iagonBaseUrl = "https://api.fireblocks.partners.iagon.com";
 export const tokenTransactionFee = 200000;
 export const MIN_RECIPIENT_LOVELACE = 1_200_000;
 export const MIN_CHANGE_LOVELACE = 1_200_000;
+
+export const STAKING_DEPOSIT_AMOUNT = 2_000_000; // 2 ADA
+export const STAKING_DEPOSIT_FEE = 300_000; // 0.3 ADA
