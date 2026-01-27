9a2d03e3563ba9afb0717e0c8e2d2f3033b4cdb9
Tomer Shoham <tshoham@fireblocks.com>
2026-01-27 17:17:47 +0200

    feat: update index.ts to include fireblocks and ensure staking export consistency

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,7 @@ export * from "./base.js";
 export * from "./config.js";
 export * from "./enums.js";
 export * from "./errors.js";
+export * from "./fireblocks.js";
 export * from "./operations.js";
 export * from "./transactions.js";
 export * from "./staking.js";
